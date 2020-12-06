3a18a18b89dbc40cc9152bbb3f0f1fdeeaafc9c4
Utsabab <utsab.khakurel@bison.howard.edu>
2020-12-05 21:40:46 -0800

    Day6 Part2 works

--- 06.py
+++ 06.py
@@ -1,3 +1,6 @@
+import collections
+
+
 def part1(data):
     ans = 0
     inpt = data.split("\n")
@@ -15,7 +18,26 @@ def part1(data):
             continue
         for j in inpt[i]:
             unique.add(j)
-
     return ans
 
-# def part2(data):
+
+def part2(data):
+    inpt = data.split("\n")
+    dict_record = collections.defaultdict(int)
+    count = 0
+    l = 0
+
+    for i in range(len(inpt)):
+        if not inpt[i]:
+            for k, v in dict_record.items():
+                if v == i - l:
+                    count += 1
+            dict_record = collections.defaultdict(int)
+            l = i + 1
+        for j in inpt[i]:
+            dict_record[j] += 1
+
+    for k, v in dict_record.items():
+        if v == i + 1 - l:
+            count += 1
+    return count
