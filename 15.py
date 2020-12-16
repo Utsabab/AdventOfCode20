52f7720cbacfcbb649175f6ef22bd195ca74ea97
Utsabab <utsab.khakurel@bison.howard.edu>
2020-12-15 20:58:42 -0800

    Day15 Part2 works

--- 15.py
+++ 15.py
@@ -29,5 +29,34 @@ def part1(data):
 
     return stck[-1]
 
-# def part2(data):
+def part2(data):
+    stck = data.split(",")
+    for i in range(len(stck)):
+        stck[i] = int(stck[i])
+
+    dict_nums = {}
+    for ind, num in enumerate(stck):
+        dict_nums[num] = [ind + 1, ind + 1, 1]
+
+    for i in range(len(stck) + 1, 30000001):
+        curr = stck[-1]
+        if dict_nums[curr][2] == 1:
+            stck.append(0)
+            if 0 in dict_nums:
+                dict_nums[0][2] += 1
+                dict_nums[0][0] = dict_nums[0][1]
+                dict_nums[0][1] = i
+            else:
+                dict_nums[0] = [i, i, 1]
+        else:
+            temp = dict_nums[curr][1] - dict_nums[curr][0]
+            stck.append(temp)
+            if temp in dict_nums:
+                dict_nums[temp][0] = dict_nums[temp][1]
+                dict_nums[temp][1] = i
+                dict_nums[temp][2] += 1
+            else:
+                dict_nums[temp] = [i, i, 1]
+
+    return stck[-1]
 
